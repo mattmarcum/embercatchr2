00f43477c3f9098dde0abe662ad3e7a20df28a38
Matt Marcum <matt@strangelooper.com>
2015-06-14 00:18:24 -0500

    inital templates and styles

---
+++ app/controllers/application.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+    actions:{
+        toggleWebTwoDotOh: function(){
+            if( this.get('webTwoDotOh') ){
+                return this.set( 'webTwoDotOh', '');
+            }
+            this.set( 'webTwoDotOh', 'e' );
+        },
+        toggleNav: function(){
+            this.toggleProperty( 'showNav' );
+        }
+    },
+
+    webTwoDotOh: 'e',
+
+    currentPathChange: function(){
+        this.set( 'showNav', false );
+    }.observes( 'currentPath' )
+});
